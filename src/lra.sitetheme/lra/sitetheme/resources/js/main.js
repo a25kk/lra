47f4b4cb9faae35fbd93f8169db9b95743edc3b0
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-03-20 20:34:10 +0100

    Add several experimental theme components

--- src/lra.sitetheme/lra/sitetheme/resources/js/main.js
+++ src/lra.sitetheme/lra/sitetheme/resources/js/main.js
@@ -1,11 +1,12 @@
-/*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
-/*global jQuery:false, document:false */
 'use strict';
-
 (function ($) {
   $(document).ready(function () {
-    if ($('body').hasClass('lt-ie7')) {return; }
-    // Application specific javascript code goes here
+    $('input[type="password"]').showPassword('focus', {
+        // toggle: { className: 'my-toggle' }
+    });
+    var bLazy = new Blazy({
+        selector: '.b-lazy'
+    });
   }
   );
 }(jQuery));
