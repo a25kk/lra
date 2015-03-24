d2681bff31f19e06ff16b84da5b284a6c2f8d128
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-03-24 23:11:15 +0100

    Fix packaging and buildout integration
    closes #3

--- src/lra.sitecontent/setup.py
+++ src/lra.sitecontent/setup.py
@@ -10,10 +10,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-long_description = \
-    read('README.rst') + \
-    read('docs', 'CHANGELOG.rst') + \
-    read('docs', 'LICENSE.rst')
+long_description = read('README.rst')
 
 setup(
     name='lra.sitecontent',
@@ -30,9 +27,8 @@ setup(
     author_email='cb@vorwaerts-werbung.de',
     url='http://pypi.python.org/pypi/lra.sitecontent',
     license='BSD',
-    packages=find_packages('src', exclude=['ez_setup']),
+    packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['lra'],
-    package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
