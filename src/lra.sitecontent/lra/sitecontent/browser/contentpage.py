93d130c9e92a531128cc3dae6cd6729ca0530f89
Christoph Böhner-Figas <boehner@team23.de>
2017-04-13 16:06:59 +0200

    IMplement file listing view

--- src/lra.sitecontent/lra/sitecontent/browser/contentpage.py
+++ src/lra.sitecontent/lra/sitecontent/browser/contentpage.py
@@ -2,6 +2,9 @@
 """Module providing views for the folderish content page type"""
 from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
+from plone import api
+
+from Products.MimetypesRegistry.MimeTypeItem import guess_icon_path
 from zope.component import getMultiAdapter
 from zope.component import getUtility
 
@@ -23,10 +26,10 @@ class ContentPageView(BrowserView):
             return True
         return False
 
-    def display_gallery(self):
+    def display_file_list(self):
         context = aq_inner(self.context)
         try:
-            display = context.displayGallery
+            display = context.displayFileList
         except AttributeError:
             display = None
         if display is not None:
@@ -37,9 +40,9 @@ class ContentPageView(BrowserView):
         tool = getUtility(IResponsiveImagesTool)
         return tool.create(uuid)
 
-    def rendered_gallery(self):
+    def rendered_file_list(self):
         context = aq_inner(self.context)
-        template = context.restrictedTraverse('@@gallery-view')()
+        template = context.restrictedTraverse('@@file-list')()
         return template
 
 
@@ -135,3 +138,79 @@ class GalleryView(BrowserView):
             item['width'] = '1px'
             item['height'] = '1px'
         return item
+
+
+class FileListPreview(BrowserView):
+    """Preview embeddable image gallery"""
+
+    def __call__(self):
+        return self.render()
+
+    def render(self):
+        return self.index()
+
+    def rendered_file_list(self):
+        context = aq_inner(self.context)
+        template = context.restrictedTraverse('@@file-list')()
+        return template
+
+
+class FileListView(BrowserView):
+
+    def __call__(self):
+        self.has_assets = len(self.contained_files()) > 0
+        return self.render()
+
+    def render(self):
+        return self.index()
+
+    def contained_files(self):
+        context = aq_inner(self.context)
+        data = context.restrictedTraverse('@@folderListing')(
+            portal_type='File',
+            sort_on='getObjPositionInParent')
+        return data
+
+    def get_file_details(self, content_file):
+        file_object = api.content.get(UID=content_file.UID)
+        attachment = getattr(file_object, 'file', None)
+        file_url = file_object.absolute_url()
+        file_name = attachment.filename
+        if attachment:
+            details = {
+                'file_name': file_name,
+                'file_url': file_url,
+                'is_video_type': self.is_video_type(attachment),
+                'is_audio_type': self.is_video_type(attachment),
+                'mime_icon': self.get_mime_type_icon(attachment),
+                'file_size': attachment.getSize() / 1024,
+                'content_type': getattr(
+                    attachment, 'contentType', 'application/octet-stream'),
+            }
+        return details
+
+    @staticmethod
+    def is_video_type(content_file):
+        ct = content_file.contentType
+        return 'video/' in ct
+
+    @staticmethod
+    def is_audio_type(content_file):
+        ct = content_file.contentType
+        return 'audio/' in ct
+
+    @staticmethod
+    def get_mime_type_icon(content_file):
+        portal_url = api.portal.get().absolute_url()
+        mtr = api.portal.get_tool("mimetypes_registry")
+        mime = []
+        if content_file.contentType:
+            mime.append(mtr.lookup(content_file.contentType))
+        if content_file.filename:
+            mime.append(mtr.lookupExtension(content_file.filename))
+        mime.append(mtr.lookup("application/octet-stream")[0])
+        icon_paths = [m.icon_path for m in mime if hasattr(m, 'icon_path')]
+        if icon_paths:
+            return icon_paths[0]
+
+        return portal_url + "/" + guess_icon_path(mime[0])
--- src/lra.sitecontent/lra/sitecontent/contentpage.py
+++ src/lra.sitecontent/lra/sitecontent/contentpage.py
@@ -4,13 +4,23 @@
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from plone.namedfile.interfaces import IImageScaleTraversable
+from zope import schema
 from zope.interface import implementer
+
+from lra.sitecontent import _
 
 
 class IContentPage(model.Schema, IImageScaleTraversable):
     """
-    A folderish page
+    A container based content page
     """
+    displayFileList = schema.Bool(
+        title=_(u"Check to enable file listing"),
+        description=_(u"When activated the view will attempt to display an "
+                      u"automatic listing of all contained files with "
+                      u"file information and download link."),
+        required=False,
+    )
 
 
 @implementer(IContentPage)
